4a61ce2bfc4779aa1bb1ca18e5f3222604896490
Rune Eerdekens <EerdekensInc@gmail.com>
2024-02-22 20:53:05 +0100

    ???????

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import gym_trading_env
 # Load CSV into DataFrame
 df = pd.read_csv('EURUSD_M1.csv', header=None, names=["timestamp", "open", "high", "low", "close", "volume"])
 
-# Print the columns to check if they match your expectations
-print(df.columns)
-
 # Calculate Required Features
 df["feature_pct_change"] = df["close"].pct_change() 
 df["feature_open"] = df["open"]/df["close"]
@@ -22,8 +19,9 @@ df.reset_index(inplace=True)  # Reset index
 df['timestamp'] = pd.to_datetime(df['timestamp'])  # Convert timestamp to datetime
 
 # Set up Gym Environment
-env = gymnasium.make("TradingEnv", df=df, positions=[-1, 0, 1], initial_position='random', portfolio_initial_value=5, max_episode_duration=10000)
-
+env = gymnasium.make("TradingEnv", df = df, positions = [-0.5, 0, 0.5], initial_position= 0, max_episode_duration = 'max', portfolio_initial_value = 1000000, verbose = 2)
+env.unwrapped.add_metric('Position Changes', lambda history : np.sum(np.diff(history['position']) != 0) )
+env.unwrapped.add_metric('Episode Lenght', lambda history : len(history['position']) )
 
 for _ in range(1):
     done = False
@@ -32,5 +30,9 @@ for _ in range(1):
     while not done and not truncated:
         action = env.action_space.sample()
         observation, reward, done, truncated, info = env.step(action)
+    if done:
+        print("ENDED CUS DONEZO")
+    if truncated: 
+        print("ENDED CUS NO MORE DAATAA")
     env.unwrapped.save_for_render(dir="render_logs")
 env.close()
