187f600d5105bd74b3d6b3bf25ac0e4bc8c5af1f
frederiquec <covent68@gmail.com>
2024-02-22 18:12:26 +0100

    removed data

--- main.py
+++ main.py
@@ -5,32 +5,37 @@ import gym_trading_env
 # Load CSV into DataFrame
 df = pd.read_csv('moneymakingmachine\EURUSD_M1.csv', header=None, names=["timestamp", "open", "high", "low", "close", "volume"])
 
-# Print the columns to check if they match your expectations
-print(df.columns)
+# Convert timestamp column to datetime objects
+df['timestamp'] = pd.to_datetime(df['timestamp'])
 
-# Calculate Required Features
+# Calculate additional features
 df["feature_pct_change"] = df["close"].pct_change() 
 df["feature_high"] = df["high"] / df["close"] - 1     
 df["feature_low"] = df["low"] / df["close"] - 1       
 
-# Preprocess DataFrame
+# Remove rows with NaN values
 df.dropna(inplace=True)
-df.reset_index(inplace=True)  # Reset index
-df['timestamp'] = pd.to_datetime(df['timestamp'])  # Convert timestamp to datetime
-df['date_str'] = df['timestamp'].dt.strftime("%Y-%m-%d %H:%M")  # Format timestamp column as string
 
-# Set up Gym Environment
+# Create trading environment
 env = gymnasium.make("TradingEnv", df=df, positions=[-1, 0, 1], initial_position='random', portfolio_initial_value=5, max_episode_duration=10000, verbose=1)
-observation, info = env.reset(seed=42)
 
+# Reset environment and get initial observation
+observation, info = env.reset()
+
+# Main loop
 for _ in range(1):
     done = False
     truncated = False
     while not done and not truncated:
-        action = -1 #env.action_space.sample()
+        action = -1  # Example action (you may want to change this)
         observation, reward, done, truncated, info = env.step(action)
     
+        # Reset environment if episode is done or truncated
         if done or truncated:
             observation, info = env.reset()
-    env.unwrapped.save_for_render(dir = "render_logs")
+    
+    # Save rendering data with formatted datetime
+    env.unwrapped.save_for_render(dir="render_logs")
+
+# Close environment
 env.close()
