c323d1fb6050ed245e94a9cab406085803fd9ad1
Rune Eerdekens <EerdekensInc@gmail.com>
2024-02-22 16:46:22 +0100

    tst

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import csv
+import pandas as pd
+import gymnasium
+import gym_trading_env
 
-with open('EURUSD_M1.csv') as f:
-    reader = list(csv.reader(f))
-    for row in reader[:10]:
-        row.pop(0)
-        print(row)
+# df = pd.read_csv('EURUSD_M1.csv')
+# print(df.head())
+
+env = gymnasium.make("CartPole-v1", render_mode="human")
+observation, info = env.reset(seed=42)
+for _ in range(10):
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        observation, reward, done, truncated, info = env.step(action)
+
+        if done or truncated:
+            observation, info = env.reset()
+env.close()
+
+# with open('EURUSD_M1.csv') as f:
+#     reader = list(csv.reader(f))
+#     for row in reader[:10]:
+#         row = list(map(float, row[1:]))
+#         print(row)
