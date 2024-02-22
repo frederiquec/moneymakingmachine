ef43960f15bf8878ec07e0960f9b9b0713b7b4de
frederiquec <covent68@gmail.com>
2024-02-22 17:40:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,18 +15,22 @@ df["feature_low"] = df["low"] / df["close"] - 1
 
 # Preprocess DataFrame
 df.dropna(inplace=True)
+df.reset_index(inplace=True)  # Reset index
+df['timestamp'] = pd.to_datetime(df['timestamp'])  # Convert timestamp to datetime
+df['date_str'] = df['timestamp'].dt.strftime("%Y-%m-%d %H:%M")  # Format timestamp column as string
 
 # Set up Gym Environment
-env = gymnasium.make("TradingEnv", df=df, positions=[-1, 0, 1], initial_position=1)
+env = gymnasium.make("TradingEnv", df=df, positions=[-1, 0, 1], initial_position='random', portfolio_initial_value=5, max_episode_duration=10000, verbose=1)
 observation, info = env.reset(seed=42)
 
-for _ in range(10):
+for _ in range(1):
     done = False
-    while not done:
-        action = env.action_space.sample()
+    truncated = False
+    while not done and not truncated:
+        action = -1 #env.action_space.sample()
         observation, reward, done, truncated, info = env.step(action)
     
         if done or truncated:
             observation, info = env.reset()
-    env.save_for_render(dir = "render_logs")
+    env.unwrapped.save_for_render(dir = "render_logs")
 env.close()
